75e943d26eb8ed081c7a3f7b6ec89d3df6a35020
382868331 <382868331@users.noreply.github.com>
2026-08-24 00:31:46 +0800

    test: reproduce CompositeUnitOrder scenario

---
+++ unit_scenarios_test.go
@@ -0,0 +1,22 @@
+package units
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"testing"
+	"unicode/utf8"
+)
+
+var (
+	_ = context.Background
+	_ = errors.Is
+	_ = reflect.DeepEqual
+	_ = utf8.ValidString
+)
+
+func TestUnitCompositeUnitOrder(t *testing.T) {
+	if got := UnitCompositeUnitOrder(5, 10, 0); got != 5 {
+		t.Fatalf("got %d", got)
+	}
+}
